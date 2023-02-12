3ce07854f70a89d4f8fea6a9e1d91fca10976bc9
Umair Khan <uk125896@gmail.com>
2023-02-12 18:01:25 +0530

    commit all

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ import mongoose from "mongoose";
 import cors from "cors";
 import userRoutes from "./routes/users.js";
 mongoose.set("strictQuery", false);
-import questionRoutes from './routes/Questions.js';
-import answerRoutes from './routes/Answers.js';
-import dotenv from 'dotenv';
+import questionRoutes from "./routes/Questions.js";
+import answerRoutes from "./routes/Answers.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
 
-dotenv.config({path:'./config.env'});
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 
 const app = express();
@@ -18,23 +22,46 @@ app.use(cors());
 
 
 app.use("/user", userRoutes);
-app.use('/questions',questionRoutes);
-app.use('/answer',answerRoutes)
-
+app.use("/questions", questionRoutes);
+app.use("/answer", answerRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello StackOverflow");
 });
 
+app.post("/chat", (req, res) => {
+  const question = req.body.question;
+  const openai = new OpenAIApi(configuration);
+
+  openai
+    .createCompletion({
+      model: "text-davinci-003",
+      prompt: question,
+      max_tokens: 4000,
+      temperature: 0,
+    })
+    .then((res) => {
+      return res?.data?.choices?.[0]?.text;
+    }).then(ans=>{
+      const arr= ans?.split('\n').filter(value=>value).map(value=>value.trim())
+      return arr;
+    })
+    .then((answer) => {
+      res.send({
+        answer: answer,
+        prompt:question,
+      });
+    });
+
+});
 
 const PORT = process.env.PORT || 5000;
-const DATABASE_URL = process.env.CONNECTION_URL
-
+const DATABASE_URL = process.env.CONNECTION_URL;
 
 mongoose
   .connect(DATABASE_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
 
   .then(() =>
@@ -45,6 +72,3 @@ mongoose
     )
   )
   .catch((err) => console.log(err.message));
-
-
-  
